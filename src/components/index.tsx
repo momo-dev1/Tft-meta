685cc43c19a3587364c19da86fe4ceb7ba788b1c
momolokii-dev <didy1234567@gmail.com>
2022-09-18 10:57:08 +0200

    refactor

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,9 +1,19 @@
 import Item from "./Item";
 import Comps from "./Comps";
+import NavBar from "./NavBar";
 import Search from "./Search";
 import RowGrid from "./RowGrid";
 import Champion from "./Champion";
 import Container from "./Container";
 import ChampionsTable from "./ChampionsTable";
 
-export { Item, Comps, Champion, Container, RowGrid, Search, ChampionsTable };
+export {
+  Item,
+  Comps,
+  Search,
+  NavBar,
+  RowGrid,
+  Champion,
+  Container,
+  ChampionsTable,
+};
